8eb51186428bb4e9449087c85485582592d07a27
Angel Miranda <angelmirandazambrana@gmail.com>
2017-07-02 08:34:10 +0200

    final first part, before creating namespaces

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(4000);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  socket.on('join', function (data) {
+    socket.broadcast.emit('userJoined',data);
+    socket.username=data.username;
   });
+
+  socket.on('ding', function (data, callback) { 
+      socket.broadcast.emit('ding',{username:socket.username});
+      callback('ack');
+    });
+    
+  socket.on('disconnect', function (){
+    socket.broadcast.emit('userDisconnect', {
+      username: socket.username});
+  });
+
 });
